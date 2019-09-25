dcb09a229ab396d2335eac665ad77a16f801ccf5
Nikita_Stepochkin <Nikita_Stepochkin@epam.com>
2019-09-25 19:21:23 +0300

    feat: Translation block

--- background.js
+++ background.js
@@ -1,11 +1,12 @@
+const yandexToken = "";
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-  let yandexToken = "";
     var url = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=" + yandexToken + "&text=" +
-              request.text + "&lang=ru";
+      request.text + "&lang=ru";
     fetch(url)
-    .then(response => response.text())
-    .then(text => sendResponse(text));
+      .then(response => response.text())
+      .then(text => sendResponse(text));
     return true;
-     }
+  }
 );
--- contentScript.js
+++ contentScript.js
@@ -1,11 +1,34 @@
-document.body.addEventListener('dblclick', function (e) {
-  translate(window.getSelection().toString());
+document.body.addEventListener('dblclick', function(e) {
+  let text = window.getSelection().toString();
+  chrome.runtime.sendMessage({
+    text: text
+  }, function(response) {
+    let translation = JSON.parse(response).text[0];
+    if (!isBlank(translation)) {
+      createTranslationElement(e, translation);
+    }
+  });
 });
 
-function translate(text){
-    return new Promise(function (resolve, reject) {
-        chrome.runtime.sendMessage({text: text}, function (response) {
-            console.log(JSON.parse(response).text[0]);
-        });
-    });
+document.body.addEventListener('click', function(e) {
+  let element = document.getElementById("chrome-extension-translation");
+  if (element) {
+    element.remove();
+  }
+});
+
+function createTranslationElement(event, translation) {
+  var x = event.clientX;
+  var y = event.clientY + event.target.offsetHeight;
+  console.log(x);
+  console.log(y);
+  var element = document.createElement("p");
+  element.style = "top:" + y + "px;left:" + x + "px;";
+  element.id = "chrome-extension-translation";
+  element.appendChild(document.createTextNode(translation));
+  document.body.appendChild(element);
+}
+
+function isBlank(str) {
+  return (!str || /^\s*$/.test(str));
 }
