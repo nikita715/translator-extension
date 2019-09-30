957fd0bb5b017ec9d96dfb9103a34b4150f00b31
Nikita Stepochkin <nikstepbox@gmail.com>
2019-09-30 20:22:00 +0300

    fix: Performance issues, cleanup

--- background.js
+++ background.js
@@ -1,63 +1,47 @@
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    let text = request.text;
-    if (request.type == "findTab") {
-      let tab = findAndUpdateTranslateTab(text);
-      if (tab) {
-        sendResponse(tab);
-        return true;
-      }
-    } else if (request.type == "createTab") {
-      let tab = createTranslateTab(text);
-      if (tab) {
-        sendResponse(tab);
-        return true;
-      }
-    }
-    return true;
-  }
-);
-
-function findAndUpdateTranslateTab(text) {
-  let newUrl = buildUrl(text);
-  chrome.tabs.query({
-    currentWindow: true,
-    url: 'https://translate.google.ru/*'
-  }, function(tabs) {
-    if (tabs[0]) {
-      let translateTab = tabs[0];
-      chrome.tabs.update(translateTab.id, {
-        'url': newUrl,
-        'active': true
-      });
-      return translateTab;
-    }
-  });
-}
-
-function createTranslateTab(text) {
-  let newUrl = buildUrl(text);
-  chrome.tabs.create({
-    'url': newUrl
-  }, function(tab) {
-    return tab;
-  });
-}
-
-function buildUrl(text) {
-  return `https://translate.google.ru/#view=home&op=translate&sl=en&tl=ru&text=${text}`;
-}
-
 chrome.contextMenus.create({
   id: "translate-selection",
   title: "Translate",
   contexts: ["selection"]
 });
 
-
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
   if (info.menuItemId == "translate-selection") {
-    let text = info.selectionText;
-    findAndUpdateTranslateTab(text);
+    translate(info.selectionText);
   }
 });
+
+chrome.runtime.onMessage.addListener(
+  function (request, sender, sendResponse) {
+    if (request.type == "translate") {
+      sendResponse(translate(request.text));
+      return true;
+    }
+  }
+);
+
+function translate(text) {
+  let newUrl = buildUrl(text);
+  chrome.tabs.query({
+    currentWindow: true,
+    url: 'https://translate.google.ru/*'
+  }, async function (tabs) {
+    let translateTab = tabs[0];
+    if (translateTab) {
+      chrome.tabs.update(translateTab.id, {
+        'url': newUrl,
+        'active': true
+      });
+      return await translateTab;
+    } else {
+      chrome.tabs.create({
+        'url': newUrl
+      }, async function (translateTab) {
+        return await translateTab;
+      });
+    }
+  });
+}
+
+function buildUrl(text) {
+  return `https://translate.google.ru/#view=home&op=translate&sl=en&tl=ru&text=${text}`;
+}
--- contentScript.js
+++ contentScript.js
@@ -1,38 +1,13 @@
-document.body.addEventListener('dblclick', function(e) {
+document.body.addEventListener('dblclick', function (e) {
   let text = window.getSelection().toString();
 
-  chrome.runtime.sendMessage({
-    type: "findTab",
-    text: text
-  }, function(response) {
-    console.log(response);
-    if (!response) {
-      chrome.runtime.sendMessage({
-        type: "createTab",
-        text: text
-      }, function(response) {
-        console.log(response);
-      });
-    }
-  });
-});
-
-document.body.addEventListener('click', function(e) {
-  let element = document.getElementById("chrome-extension-translation");
-  if (element) {
-    element.remove();
+  if (!isBlank(text)) {
+    chrome.runtime.sendMessage({
+      type: "translate",
+      text
+    });
   }
 });
-
-function createTranslationElement(event, translation) {
-  var x = event.clientX;
-  var y = event.clientY + event.target.offsetHeight;
-  var element = document.createElement("p");
-  element.style = "top:" + y + "px;left:" + x + "px;";
-  element.id = "chrome-extension-translation";
-  element.appendChild(document.createTextNode(translation));
-  document.body.appendChild(element);
-}
 
 function isBlank(str) {
   return (!str || /^\s*$/.test(str));
